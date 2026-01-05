cde90c46a27c9bf025adc4693773d4bc5f060dc3
Rodrigo Polydoro <rspolydoro@gmail.com>
2026-01-04 21:33:05 -0300

    fix: data init

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,10 +18,7 @@ INSERT INTO tb_usuario (
     senha,
     funcao,
     status,
-    deve_mudar_senha,
-    criado_em,
-    criado_por,
-    atualizado_em
+    deve_mudar_senha
 ) VALUES (
     1,
     'Sistema',
@@ -30,10 +27,7 @@ INSERT INTO tb_usuario (
     '$2a$12$disabled.password.hash.not.usable',
     'ADMIN',
     'ACTIVE',
-    false,
-    CURRENT_TIMESTAMP,
-    1,
-    CURRENT_TIMESTAMP
+    false
 )
 ON CONFLICT (id) DO NOTHING;
 
@@ -50,10 +44,7 @@ INSERT INTO tb_usuario (
     senha,
     funcao,
     status,
-    deve_mudar_senha,
-    criado_em,
-    criado_por,
-    atualizado_em
+    deve_mudar_senha
 ) VALUES (
     'Admin',
     'Sistema',
@@ -61,10 +52,7 @@ INSERT INTO tb_usuario (
     '$2a$12$wVByGsG.Ko94ePxBn/dTt.sTzh7RRXYkRH.P2TYKEo.8HjhyvOI9.',
     'ADMIN',
     'ACTIVE',
-    true,
-    CURRENT_TIMESTAMP,
-    1,
-    CURRENT_TIMESTAMP
+    true
 )
 ON CONFLICT (email) DO NOTHING;
 
@@ -76,55 +64,52 @@ INSERT INTO tb_parametros_tributarios (
     codigo,
     tipo,
     descricao,
-    status,
-    criado_em,
-    criado_por,
-    atualizado_em
+    status
 ) VALUES
-    ('1', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Real', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('2', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Real/Arbitrado', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('3', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Presumido/Real', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('4', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Presumido/Real/Arbitrado', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('5', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Presumido', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('6', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Arbitrado', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('7', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Presumido/Arbitrado', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('8', 'FORMA_TRIB_LUCRO_REAL', 'Imune de IRPJ', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('9', 'FORMA_TRIB_LUCRO_REAL', 'Isento do IRPJ', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('A', 'PERIODO_DE_APURACAO', 'Anual', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('T', 'PERIODO_DE_APURACAO', 'Trimestral', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('01', 'QUALIFICACAO_PESSOA_JURIDICA', 'PJ em Geral', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('02', 'QUALIFICACAO_PESSOA_JURIDICA', 'PJ Componente do Sistema Financeiro', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('03', 'QUALIFICACAO_PESSOA_JURIDICA', 'Sociedades Seguradoras, de Capitalização e Previdência', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1', 'CRITERIO_RECONHECIMENTO__RECEITA', 'Regime de caixa', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('2', 'CRITERIO_RECONHECIMENTO__RECEITA', 'Regime de competência', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1', 'ESTIMATIVA_MENSAL', 'Receita Bruta e Acréscimos', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('2', 'ESTIMATIVA_MENSAL', 'Balanço/Balancete de Suspensão/Redução', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('P', 'FORMA_TRIBUTACAO', 'Presumido', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('R', 'FORMA_TRIBUTACAO', 'Real', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('A', 'FORMA_TRIBUTACAO', 'Arbitrado', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1015', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Executivo Federal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1023', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Executivo Estadual ou do Distrito Federal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1031', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Executivo Municipal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1040', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Legislativo Federal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1058', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Legislativo Estadual ou do Distrito Federal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1066', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Legislativo Municipal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1074', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Judiciário Federal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1082', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Judiciário Estadual', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1104', 'NATUREZA_JURIDICA', 'Autarquia Federal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1112', 'NATUREZA_JURIDICA', 'Autarquia Estadual ou do Distrito Federal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('1120', 'NATUREZA_JURIDICA', 'Autarquia Municipal', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0111301', 'CNAE', 'Cultivo de arroz', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0111302', 'CNAE', 'Cultivo de milho', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0111303', 'CNAE', 'Cultivo de trigo', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0111399', 'CNAE', 'Cultivo de outros cereais não especificados anteriormente', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0112101', 'CNAE', 'Cultivo de algodão herbáceo', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0112102', 'CNAE', 'Cultivo de juta', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0112199', 'CNAE', 'Cultivo de outras fibras de lavoura temporária não especificadas anteriormente', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0113000', 'CNAE', 'Cultivo de cana-de-açúcar', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0114800', 'CNAE', 'Cultivo de fumo', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0115600', 'CNAE', 'Cultivo de soja', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0116401', 'CNAE', 'Cultivo de amendoim', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP),
-    ('0116402', 'CNAE', 'Cultivo de girassol', 'ACTIVE', CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP)
+    ('1', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Real', 'ACTIVE'),
+    ('2', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Real/Arbitrado', 'ACTIVE'),
+    ('3', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Presumido/Real', 'ACTIVE'),
+    ('4', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Presumido/Real/Arbitrado', 'ACTIVE'),
+    ('5', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Presumido', 'ACTIVE'),
+    ('6', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Arbitrado', 'ACTIVE'),
+    ('7', 'FORMA_TRIB_LUCRO_REAL', 'Lucro Presumido/Arbitrado', 'ACTIVE'),
+    ('8', 'FORMA_TRIB_LUCRO_REAL', 'Imune de IRPJ', 'ACTIVE'),
+    ('9', 'FORMA_TRIB_LUCRO_REAL', 'Isento do IRPJ', 'ACTIVE'),
+    ('A', 'PERIODO_DE_APURACAO', 'Anual', 'ACTIVE'),
+    ('T', 'PERIODO_DE_APURACAO', 'Trimestral', 'ACTIVE'),
+    ('01', 'QUALIFICACAO_PESSOA_JURIDICA', 'PJ em Geral', 'ACTIVE'),
+    ('02', 'QUALIFICACAO_PESSOA_JURIDICA', 'PJ Componente do Sistema Financeiro', 'ACTIVE'),
+    ('03', 'QUALIFICACAO_PESSOA_JURIDICA', 'Sociedades Seguradoras, de Capitalização e Previdência', 'ACTIVE'),
+    ('1', 'CRITERIO_RECONHECIMENTO__RECEITA', 'Regime de caixa', 'ACTIVE'),
+    ('2', 'CRITERIO_RECONHECIMENTO__RECEITA', 'Regime de competência', 'ACTIVE'),
+    ('1', 'ESTIMATIVA_MENSAL', 'Receita Bruta e Acréscimos', 'ACTIVE'),
+    ('2', 'ESTIMATIVA_MENSAL', 'Balanço/Balancete de Suspensão/Redução', 'ACTIVE'),
+    ('P', 'FORMA_TRIBUTACAO', 'Presumido', 'ACTIVE'),
+    ('R', 'FORMA_TRIBUTACAO', 'Real', 'ACTIVE'),
+    ('A', 'FORMA_TRIBUTACAO', 'Arbitrado', 'ACTIVE'),
+    ('1015', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Executivo Federal', 'ACTIVE'),
+    ('1023', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Executivo Estadual ou do Distrito Federal', 'ACTIVE'),
+    ('1031', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Executivo Municipal', 'ACTIVE'),
+    ('1040', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Legislativo Federal', 'ACTIVE'),
+    ('1058', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Legislativo Estadual ou do Distrito Federal', 'ACTIVE'),
+    ('1066', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Legislativo Municipal', 'ACTIVE'),
+    ('1074', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Judiciário Federal', 'ACTIVE'),
+    ('1082', 'NATUREZA_JURIDICA', 'Órgão Público do Poder Judiciário Estadual', 'ACTIVE'),
+    ('1104', 'NATUREZA_JURIDICA', 'Autarquia Federal', 'ACTIVE'),
+    ('1112', 'NATUREZA_JURIDICA', 'Autarquia Estadual ou do Distrito Federal', 'ACTIVE'),
+    ('1120', 'NATUREZA_JURIDICA', 'Autarquia Municipal', 'ACTIVE'),
+    ('0111301', 'CNAE', 'Cultivo de arroz', 'ACTIVE'),
+    ('0111302', 'CNAE', 'Cultivo de milho', 'ACTIVE'),
+    ('0111303', 'CNAE', 'Cultivo de trigo', 'ACTIVE'),
+    ('0111399', 'CNAE', 'Cultivo de outros cereais não especificados anteriormente', 'ACTIVE'),
+    ('0112101', 'CNAE', 'Cultivo de algodão herbáceo', 'ACTIVE'),
+    ('0112102', 'CNAE', 'Cultivo de juta', 'ACTIVE'),
+    ('0112199', 'CNAE', 'Cultivo de outras fibras de lavoura temporária não especificadas anteriormente', 'ACTIVE'),
+    ('0113000', 'CNAE', 'Cultivo de cana-de-açúcar', 'ACTIVE'),
+    ('0114800', 'CNAE', 'Cultivo de fumo', 'ACTIVE'),
+    ('0115600', 'CNAE', 'Cultivo de soja', 'ACTIVE'),
+    ('0116401', 'CNAE', 'Cultivo de amendoim', 'ACTIVE'),
+    ('0116402', 'CNAE', 'Cultivo de girassol', 'ACTIVE')
 ON CONFLICT (codigo, tipo) DO NOTHING;
 
 -- ============================================================================
